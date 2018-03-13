c292b7f4309215809eebfc65585e8d3ed5f97208
Carolyn Smith <casmith205@gmail.com>
2018-03-13 10:13:40 -0500

    added questions and answers array

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+var questions = [
+    "What is the name of the OSU student section?",
+    "What is the state tree of Ohio?",
+    "What is the OSU mascot's name?",
+    "Who was the OSU football coach who punched a Clemson player in the Gator Bowl in 1978?",
+    "Who was the only two-time Heisman Trophy winner from Ohio State?",
+    "What does TBDBITL stand for in regards to the OSU Marching Band?",
+    "What famous comedian from Cleveland, Ohio dotted the 'I'?",
+    "How do you respond to an OSU fan who yells 'O-H!'?",
+    "What 1960s song by the McCoys became an Ohio State staple?"
+];
+
+var answers = [
+    "Block-O",
+    "Buckeye Tree",
+    "Brutus Buckeye",
+    "Woody Hayes",
+    "Archie Griffin",
+    "The Best Damn Band in the Land",
+    "Bob Hope",
+    "I-O!",
+    "Hang on Sloopy"
+]
+
+var choices = {
+    q1 : ["Buckeye City", "Block-O", "The Brutus House", "The Angry Tree Nuts"],
+    q2 : ["Buckeye", "Boxelder Maple", "Alder", "Blackhaw"],
+    q3 : ["Betty Buckeye", "Bruce Buckeye", "Brutus Buckeye", "Bill Buckeye"],
+    q4 : ["Woody Hayes"],
+    q5 : ["Archie Griffin"],
+    q6 : ["The Best Damn Band in the Land"],
+    q7 : ["Bob Hope"],
+    q8 : ["I-O!"],
+    q9 : ["Hang on Sloopy"],
+}
+
+
